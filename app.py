825673612453442f3a3c925984338028d6f4ea94
789william@gmail.com <789william@gmail.com>
2024-05-16 20:54:59 +0800

    上傳檔案

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text=msg)
+        message = TextSendMessage(text='你說的是不是'+msg)
         line_bot_api.reply_message(event.reply_token, message)
 
 @handler.add(PostbackEvent)
