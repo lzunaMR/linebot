f354d1fb6104226b9026ee4e3f0d48b8bb21fad7
789william@gmail.com <789william@gmail.com>
2024-05-19 01:44:05 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -4,7 +4,7 @@
 from pymongo.mongo_client import MongoClient
 uri = "mongodb+srv://789william:123Vanoss@cluster0.binj4fs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
-client = MongoClient(uri)
+client = MongoClient(uri,ssl=True)
 
 
 #第一個db的建立
