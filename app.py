8541e782ec5c9ebd2579bef90979d11fd96f9a1c
789william@gmail.com <789william@gmail.com>
2024-07-08 23:50:43 +0800

    change_awake

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def check_reminders():
 if __name__ == "__main__":
     start_reminder_thread()  # 启动提醒线程
     # 启动保持 Render 唤醒线程
-    keep_awake_thread = threading.Thread(target=keep_render_awake.run_schedule)
+    keep_awake_thread = threading.Thread(target=keep_render_awake)
     logger.info("Starting keep awake thread...")
     keep_awake_thread.start()
     port = int(os.environ.get('PORT', 5000))
