40d56579e395aa40ef33bc5a02b4100181d3f67e
789william@gmail.com <789william@gmail.com>
2024-07-09 15:38:41 +0800

    c

--- keep_render_awake.py
+++ keep_render_awake.py
@@ -12,7 +12,7 @@ def keep_render_awake():
         current_time = datetime.datetime.now().time()
         if current_time < datetime.time(0, 0) or current_time >= datetime.time(7, 0):
             try:
-                response = requests.get("https://linebot-28fd.onrender.com")
+                response = requests.get("https://linebot-db1b.onrender.com")
                 if response.status_code == 200:
                     logger.info("Render is awake")
                 else:
